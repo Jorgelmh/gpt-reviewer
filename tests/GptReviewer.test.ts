552a0414437e190360f071d3f9b79eed3b57df97
Jorge Mendez <jorgelmh_01@hotmail.com>
2023-07-16 16:30:31 -0400

    Gpt review code and format response for UI display

--- tests/GptReviewer.test.ts
+++ tests/GptReviewer.test.ts
@@ -1,5 +1,49 @@
-import { assertEquals } from '$std/testing/asserts.ts'
+import { assert, assertEquals, assertStringIncludes } from '$std/testing/asserts.ts'
+import GptReviewer from '../utils/GptReviewer.ts'
+import { separateCodeFromResponse } from '../utils/util.ts'
 
 Deno.test('First test', () => {
   assertEquals(1, 1)
+})
+
+Deno.test('Should separate the code and the text messages', () => {
+  const gptMessage = "The code is: ```javascript\n const val = 'hello' \n```"
+  const { language, messages } = separateCodeFromResponse(gptMessage)
+
+  assertEquals(language, 'javascript')
+  assertEquals(messages.length, 3)
+})
+
+Deno.test('Should return corrected code to a flawed algorithm', async () => {
+  const gpt = new GptReviewer('sadasd')
+  const code = `function bubbleSort(arr, n)
+  {
+     var i, j, temp;
+     var swapped;
+     for (i = 0; i < n - 1; i++) 
+     {
+         swapped = false;
+         for (j = 0; j < n - i - 1; j++) 
+         {
+             if (arr[j] < arr[j + 1]) 
+             {
+                 // Swap arr[j] and arr[j+1]
+                 temp = arr[j];
+                 arr[j] = arr[j + 1];
+                 arr[j + 1] = temp;
+                 swapped = true;
+             }
+         }
+   
+         // IF no two elements were 
+         // swapped by inner loop, then break
+         if (swapped == false)
+         break;
+     }
+ }`
+  const userExplanation = 'Implements the bubble sort algorithm in JavaScript'
+  const review = await gpt.generateReview(code, userExplanation)
+
+  assertStringIncludes(review.prompt, userExplanation)
+  assertEquals(review.language, 'javascript')
 })
---
+++ utils/util.ts
@@ -0,0 +1,29 @@
+import { GptMessageSection, MessageSection } from '../types/GptTypes.ts'
+import { v1 } from '$std/uuid/mod.ts'
+
+/**
+ * Separate code from string and return eberything as a list
+ * 
+ * @param message {string} -> Response from ChatGpt
+ */
+export const separateCodeFromResponse = (
+    message: string
+  ) : { messages: GptMessageSection[], language: string} => {
+  const regex = /```[\w\s]*\n([\s\S]*?)\n```/
+  const matches = message.split(regex)
+  const language = message.match(/```(\w+)\n/)
+
+  const messages : GptMessageSection[] = matches.map((val, index) => {
+    const msgType = (index % 2 === 0) ? MessageSection.TEXT : MessageSection.CODE
+    return {
+      id: <string> v1.generate(),
+      type: msgType,
+      content: val
+    }
+  })
+
+  return {
+    messages,
+    language: (language) ? language[1] : 'N/a'
+  }
+}
